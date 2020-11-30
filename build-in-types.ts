8e1b265e3bc1b94536b8f12c5306cd8c3e6400ba
zhz <1456421562@qq.com>
2020-11-30 18:44:57 +0800

    feat 内置类型

--- build-in-types.ts
+++ build-in-types.ts
@@ -1,11 +1,11 @@
-const a: Array<number> = [1,2,3]
+const a: Array<number> = [1, 2, 3]
 const date = new Date()
 date.getTime()
 const reg = /abc/
 reg.test('abc')
 
 
-Math.pow(2,2)
+Math.pow(2, 2)
 
 
 let body = document.body
@@ -13,5 +13,16 @@ let body = document.body
 let allLis = document.querySelectorAll('li')
 allLis.keys()
 document.addEventListener('click', () => {
-  
+
 })
+
+interface IPerson {
+  name: string
+  age: number
+}
+let zhz: IPerson = { name: 'zhz', age: 26 }
+type IPartial = Partial<IPerson>
+let zhz2: IPartial = {name :'zhz'}
+type IOmit = Omit<IPerson, 'name'>
+let zhz3: IOmit = {age:20}
+
