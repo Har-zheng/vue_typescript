d6ce6a0abfd32511106d2187eb20af32f4976cff
unknown <1456421562@qq.com>
2020-12-05 00:31:06 +0800

    feat css样式美化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
+
+
+/*
+找到自己本身错误的原因在这里特别记录一下（未能正确自动格式化）
+1  未在vscode右下角开启Eslint  需要点击下
+'extends': [
+  'plugin:vue/vue3-essential',
+  '@vue/standard',
+  '@vue/typescript/recommended'
+],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+*/  
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    es6: true,
   },
   'extends': [
     'plugin:vue/vue3-essential',
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
-  parserOptions: {
+  parserOptions: { 
     ecmaVersion: 2020
   },
   rules: {
--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,54 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import ColumnList, { ColumnProps } from "./components/ColumnList.vue";
+import { defineComponent } from 'vue'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 const testData: ColumnProps[] = [
   {
     id: 1,
-    title: "test1的专栏",
-    description: "这里是专栏一 ，一个有趣的简介",
-    avatar: "https://images.dog.ceo/breeds/maltese/n02085936_10073.jpg",
+    title: 'test1的专栏',
+    description: '这里是专栏一 ，一个有趣的简介',
+    avatar: 'https://images.dog.ceo/breeds/maltese/n02085936_10073.jpg'
   },
   {
-    id: 1,
-    title: "test2的专栏",
-    description: "这里是专栏二 ，一个有趣的简介",
-    avatar: "https://images.dog.ceo/breeds/maltese/n02085936_10073.jpg",
+    id: 2,
+    title: 'test2的专栏',
+    description: '这里是专栏二 ，一个有趣的简介',
+    avatar: 'https://images.dog.ceo/breeds/maltese/n02085936_10073.jpg'
+
   },
-];
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这里是专栏二 ，一个有趣的简介',
+    avatar: 'https://images.dog.ceo/breeds/maltese/n02085936_10073.jpg'
+
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这里是专栏二 ，一个有趣的简介',
+    avatar: 'https://images.dog.ceo/breeds/maltese/n02085936_10073.jpg'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这里是专栏二 ，一个有趣的简介',
+    avatar: 'https://images.dog.ceo/breeds/maltese/n02085936_10073.jpg'
+  }
+]
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    ColumnList,
+    ColumnList
   },
-  setup() {
+  setup () {
     return {
-      list: testData,
-    };
-  },
-});
+      list: testData
+    }
+  }
+})
 </script>
 
 <style>
