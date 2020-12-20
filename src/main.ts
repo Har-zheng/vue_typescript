f3b6dde853968556ce3353e05b389b6bab81b8cd
unknown <1456421562@qq.com>
2020-12-20 23:56:49 +0800

    feat  vuex

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './route/router'
-import { createStore } from 'vuex'
-const store = createStore({
-  state: {
-    count:0
-  },
-  mutations: {
-    add(state){
-      state.count++
-    }
-  }
-})
-store.commit('add')
-console.log('store', store.state.count)
-createApp(App).use(router).mount('#app')
+import  store from './store/store'
+const app = createApp(App)
+app.use(router)
+app.use(store)
+app.mount('#app')
---
+++ src/store/store.ts
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+import { testData, testPosts, PostProps, ColumnProps } from '../views/testDatas'
+
+interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number
+}
+export interface GlobalDataProps {
+  columns: ColumnProps[];
+  posts: PostProps[];
+  user: UserProps
+}
+const store = createStore<GlobalDataProps>({
+  state: {
+    columns: testData,
+    posts: testPosts,
+    user: { isLogin: false }
+  }
+})
+export default store
