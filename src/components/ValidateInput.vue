82a7eb325a0d07a15ab685a2c9f2da64e05c3f59
unknown <1456421562@qq.com>
2021-01-24 23:25:14 +0800

    feat modal input

--- src/components/ValidateInput.vue
+++ src/components/ValidateInput.vue
@@ -2,104 +2,89 @@
   <div class="validate-input-container pb-3">
     <input
       v-if="tag !== 'textarea'"
-      type="text"
       class="form-control"
-      :class="{ 'is-invalid': inputRef.error }"
-      :value="inputRef.val"
-      @blur="validateInput"
-      v-bind="$attrs"
-    />
-    <textarea
-      v-else
-      class="form-control"
-      :class="{ 'is-invalid': inputRef.error }"
+      :class="{'is-invalid': inputRef.error}"
       @blur="validateInput"
       v-model="inputRef.val"
       v-bind="$attrs"
-    />
-    {{ inputRef }}
-    <span v-if="inputRef.error" class="invalid-feedback">
-      {{ inputRef.message }}
-    </span>
+    >
+    <textarea
+      v-else
+      class="form-control"
+      :class="{'is-invalid': inputRef.error}"
+      @blur="validateInput"
+      v-model="inputRef.val"
+      v-bind="$attrs"
+    >
+    </textarea>
+    <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
   </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent, reactive, PropType, onMounted } from "vue";
-import { emitter } from "./ValidateFrom.vue";
+<script lang="ts">
+import { defineComponent, reactive, PropType, onMounted, computed } from 'vue'
+import { emitter } from './ValidateForm.vue'
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 interface RuleProp {
-  type: "required" | "email" | "password" | "custom";
+  type: 'required' | 'email' | 'custom';
   message: string;
   validator?: () => boolean;
 }
-const emailReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
-export type RulesProp = RuleProp[];
-export type TagType = "input" | "textarea";
+export type RulesProp = RuleProp[]
+export type TagType = 'input' | 'textarea'
 export default defineComponent({
   props: {
     rules: Array as PropType<RulesProp>,
     modelValue: String,
-    textarea: String,
     tag: {
       type: String as PropType<TagType>,
-      default: "input",
-    },
+      default: 'input'
+    }
   },
   inheritAttrs: false,
   setup(props, context) {
-    // 为解决响应式数据的监听页面响应   这个vue2.X时的 $set 相类似
     const inputRef = reactive({
-      val: "",
+      val: computed({
+        get: () => props.modelValue || '',
+        set: val => {
+          context.emit('update:modelValue', val)
+        }
+      }),
       error: false,
-      message: "",
-    });
-    const updateValue = (e: KeyboardEvent) => {
-      const targetValue = (e.target as HTMLInputElement).value;
-      console.log(targetValue);
-      inputRef.val = targetValue;
-      context.emit("update:modelValue", targetValue);
-    };
-    const validateInput = (e: KeyboardEvent) => {
-      const targetValue = (e.target as HTMLInputElement).value;
-      console.log(targetValue);
-      inputRef.val = targetValue;
-      context.emit("update:modelValue", inputRef.val);
+      message: ''
+    })
+    const validateInput = () => {
       if (props.rules) {
-        const allPassed = props.rules.every((rule) => {
-          let passed = true;
-          inputRef.message = rule.message;
+        const allPassed = props.rules.every(rule => {
+          let passed = true
+          inputRef.message = rule.message
           switch (rule.type) {
-            case "password":
-              passed = inputRef.val.trim() !== "" && inputRef.val.length >= 6;
-              break;
-            case "email":
-              passed = emailReg.test(inputRef.val);
-              break;
-            case "custom":
-              passed = rule.validator ? rule.validator() : true;
-              break;
+            case 'required':
+              passed = (inputRef.val.trim() !== '')
+              break
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break
+            case 'custom':
+              passed = rule.validator ? rule.validator() : true
+              break
             default:
-              break;
+              break
           }
-          return passed;
-        });
-        inputRef.error = !allPassed;
-
-        return allPassed;
+          return passed
+        })
+        inputRef.error = !allPassed
+        return allPassed
       }
-      return true;
-    };
+      return true
+    }
     onMounted(() => {
-      emitter.emit("form-item-created", inputRef.val);
-    });
+      emitter.emit('form-item-created', validateInput)
+    })
     return {
       inputRef,
-      validateInput,
-      updateValue,
-    };
-  },
-});
+      validateInput
+    }
+  }
+})
 </script>
-
-<style>
-</style>
